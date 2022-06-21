154c6a8c7b2ab276e8cc9a843d92312008ba0a61
Medachii <107952303+Medachii@users.noreply.github.com>
2022-06-21 20:51:59 +0200

    v1 Program

---
+++ cassetete.py
@@ -0,0 +1,156 @@
+import json
+import time
+def creaTab(i,j):
+    T=[] 
+    for k in range(i):
+        L=[0 for k in range(j)]
+        T.append(L)
+    return T
+
+def mettre(T,i,j,obj):
+#    t=creaTab(len(T),len(T[0]))
+#    for k in range(i):
+#        for l in range(j):
+#            t[k][l]=T[k][l]
+    #print(t)
+    for k in range(len(obj)):
+        I=i+obj[k][0]
+        J=j+obj[k][1]
+        if I>len(T)-1 or J>len(T[0])-1 or I<0 or J<0:
+            pass
+            #return T
+        else:
+            T[I][J]+=1
+        #print(T,t)
+def mettretest(T,i,j,obj):
+    a=0
+    for k in range(len(obj)):
+        I=i+obj[k][0]
+        J=j+obj[k][1]
+        if I>len(T)-1 or J>len(T[0])-1 or I<0 or J<0:
+            a+=1
+    if a!=0:
+        return False
+    return True
+
+def retire(T,i,j,obj):
+#    t=creaTab(len(T),len(T[0]))
+#    for k in range(i):
+#        for l in range(j):
+#            t[k][l]=T[k][l]
+    #print(t)
+    for k in range(len(obj)):
+        I=i+obj[k][0]
+        J=j+obj[k][1]
+        if I>len(T)-1 or J>len(T[0])-1 or I<0 or J<0:
+            pass
+            #return T
+        else:
+            T[I][J]-=1
+        #print(T,t)
+
+def surpasse(T):
+    a=0
+    for i in range(len(T)):
+        for j in range(len(T[0])):
+            if T[i][j]>1:
+                a+=1
+    if a!=0:
+        return True
+    else:
+        return False
+        
+
+def estComplet(T):
+    a=0
+    for i in range(len(T)):
+        for j in range(len(T[0])):
+            if T[i][j]!=1:
+                a+=1
+    if a!=0:
+        return False
+    else:
+        return True
+def chercheLibre(T):
+    LIBRE=[]
+    for i in range(len(T)):
+        for j in range(len(T[0])):
+            if T[i][j]==0:
+                LIBRE.append([i,j])
+    return LIBRE
+def copie(T):
+     L=creaTab(len(T),len(T[0]))
+     for k in range(len(T)):
+         for l in range(len(T[0])):
+             L[k][l]=T[k][l]
+     return L
+    
+        
+def solution(T,objets):
+    global solu,histo,H
+    solu=creaTab(len(T),len(T[0]))
+    histo=[]
+    historique=[]
+    H=0
+    #print("1ere",solu)
+    P=len(objets)
+    def soluRec(profondeur,courante,historique):
+        global solu,histo,H
+        #print(courante)
+        if H==1:
+            return None
+        if surpasse(courante) or profondeur==P:
+            return None
+        else:
+            LIBRE=chercheLibre(courante)
+            
+            for k in range(len(LIBRE)):
+                if mettretest(courante,LIBRE[k][0],LIBRE[k][1],objets[profondeur]):
+                    mettre(courante,LIBRE[k][0],LIBRE[k][1],objets[profondeur])
+                    historique.append([profondeur,LIBRE[k][0],LIBRE[k][1]])
+                    #print(histo)
+                    
+                
+                
+                    if estComplet(courante):
+                        print("trouvée")
+                        solu=copie(courante)
+                        histo=copie(historique)
+                        H=1
+                    
+                    soluRec(profondeur+1,courante,historique)
+                
+                    retire(courante,LIBRE[k][0],LIBRE[k][1],objets[profondeur])
+                    del historique[-1]
+                    soluRec(profondeur+1,courante,historique)
+        
+    soluRec(0,T,historique)
+    return histo
+        
+def lecture(objets):
+    f=open("cataforme.txt","r")
+    R=f.readlines()
+    A=[]
+    B=[]
+    
+    for k in R:
+        A.append(int(k[:3]))
+        B.append(json.loads(k[4:-1]))
+    obj=[]
+    for k in range(len(objets)):
+        i=A.index(objets[k])
+        obj.append(B[i])
+    return obj
+
+
+
+#CreaTab et Objets: 
+T=creaTab(10,3)  #lignes colonnes
+objets=[31,37,19,9,38,36,39]
+obj=lecture(objets)
+start=time.time()
+print("solu",solution(T,[[[0,0],[0,1],[1,0],[1,1],[2,0],[2,1]],[[0,0],[0,1],[1,1],[2,1],[3,1]],[[0,0],[1,0],[2,0],[3,0],[4,0],[4,-1]],[[0,0],[1,0],[2,0],[3,0],[4,0],[5,0]],[[0,0],[1,0],[1,1],[2,0],[3,0],[4,0],[5,0]]]))
+end=time.time()
+print(end-start)
+
+    
